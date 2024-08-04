877cd48760d4999fd23a1a4d0b5298709b056869
ronnie <barakdeployment@gmail.com>
2024-08-04 16:58:42 +0300

    fix(reader.py, writer.py): renamed the generic run functions names
    humped between the names 'writer' and 'publisher' like a tennis match (during the olimpics tennis match)

--- reader.py
+++ reader.py
@@ -1,18 +1,15 @@
-from builtins import function
-from sys import maxsize
 import rti.asyncio
 import rti.connextdds as dds
 from loguru import logger
-
 
 import constants as const
 from Configuration.TopicData import StructEnum, topic_data_dict
 
 
-class ProfilesExampleSubscriber:
+class Subscriber:
     def __init__(self, struct_enum: StructEnum, subscribe_event):
-        topic_name = topic_data_dict[struct_enum].topic_name
-        topic_struct = topic_data_dict[struct_enum].topic_struct
+        self.topic_name = topic_data_dict[struct_enum].topic_name
+        self.topic_struct = topic_data_dict[struct_enum].topic_struct
 
         qos_provider = dds.QosProvider(const.QOS_PROVIDER)
         self.participant = dds.DomainParticipant(
@@ -23,8 +20,8 @@ class ProfilesExampleSubscriber:
         )
         self.topic = dds.Topic(
             self.participant,
-            topic_name,
-            topic_struct,
+            self.topic_name,
+            self.topic_struct,
             qos_provider.topic_qos,
         )
         self.reader_default = dds.DataReader(
@@ -35,24 +32,17 @@ class ProfilesExampleSubscriber:
             ),
         )
         self.subscribe_event = subscribe_event
-        self.samples_read = 0
 
-    def execute_subscribe_event(self, *args, **kwargs):
+    def __execute_subscribe_event(self, *args, **kwargs):
         return self.subscribe_event(*args, **kwargs)
 
-    async def run(self, sample_count: int):
-
+    async def __run(self):
         async for data in self.reader_default.take_data_async():
-            self.execute_subscribe_event(data)
+            self.__execute_subscribe_event(data)
             logger.trace(f'{data}')
-            self.samples_read += 1
-            if self.samples_read >= sample_count:
-                break
 
-
-def run(struct_enum: StructEnum, subscribe_event: function):
-    try:
-        subscriber = ProfilesExampleSubscriber(struct_enum, subscribe_event)
-        rti.asyncio.run(subscriber.run(maxsize))
-    except KeyboardInterrupt:
-        pass
+    def subscribe(self):
+        try:
+            rti.asyncio.run(self.__run())
+        except KeyboardInterrupt:
+            pass
--- writer.py
+++ writer.py
@@ -4,13 +4,12 @@ from rti import idl
 
 import constants
 from Configuration.TopicData import StructEnum, topic_data_dict
-from auto_idl_types.DetectionOptronics import P_Tactical_Sensor_PSM_C_Detection_Optronics
 
 
-class ProfilesExamplePublisher:
+class Publisher:
     def __init__(self, struct_enum: StructEnum):
-        topic_name = topic_data_dict[struct_enum].topic_name
-        topic_struct = topic_data_dict[struct_enum].topic_struct
+        self.topic_name = topic_data_dict[struct_enum].topic_name
+        self.topic_struct = topic_data_dict[struct_enum].topic_struct
         qos_provider = dds.QosProvider(constants.QOS_PROVIDER)
 
         self.participant = dds.DomainParticipant(
@@ -19,8 +18,8 @@ class ProfilesExamplePublisher:
         publisher = dds.Publisher(self.participant, qos_provider.publisher_qos)
         topic = dds.Topic(
             self.participant,
-            topic_name,
-            topic_struct,
+            self.topic_name,
+            self.topic_struct,
             qos_provider.topic_qos,
         )
         self.writer_default = dds.DataWriter(
@@ -30,14 +29,12 @@ class ProfilesExamplePublisher:
                 constants.PROFILE_NAME
             ),
         )
-        self.samples_written = 0
 
-    async def run(self, struct_to_write):
-        self.writer_default.write(struct_to_write)
+    async def __run(self, struct_to_publish):
+        self.writer_default.write(struct_to_publish)
 
-
-def write(struct_enum: StructEnum, struct_to_write: idl.struct):
-    try:
-        rti.asyncio.run(ProfilesExamplePublisher(struct_enum).run(struct_to_write))
-    except KeyboardInterrupt:
-        pass
+    def publish(self, struct_to_publish: idl.struct):
+        try:
+            rti.asyncio.run(self.__run(struct_to_publish))
+        except KeyboardInterrupt:
+            pass
