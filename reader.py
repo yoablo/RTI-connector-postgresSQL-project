2d51cc673eaa04156b6c1e9efc021f3cd2584e4d
Rei Naftali <rehi12380@gmail.com>
2024-08-05 14:45:12 +0300

    Feat(reader.py): merge fix

--- reader.py
+++ reader.py
@@ -1,13 +1,11 @@
 import rti.asyncio
-import rti.connextdds as dds
 import rti.idl as idl
-
+import rti.connextdds as dds
+from loguru import logger
+from typing import Callable
+from Configuration.TopicData import StructEnum, topic_data_dict
 from auto_idl_types.LDM_Common import P_LDM_Common_T_Identifier
 from constants import DEFAULT_DOMAIN_ID, QOS_PROVIDER, PROFILE_NAME
-
-from typing import Callable
-from loguru import logger
-from Configuration.TopicData import StructEnum, topic_data_dict
 
 
 class Subscriber:
@@ -15,6 +13,7 @@ class Subscriber:
                  filter_keys: list[P_LDM_Common_T_Identifier], domain_id=DEFAULT_DOMAIN_ID):
         self.topic_name = topic_data_dict[struct_enum].topic_name
         self.topic_struct = topic_data_dict[struct_enum].topic_struct
+        self.filter_keys = filter_keys
 
         qos_provider = dds.QosProvider(QOS_PROVIDER)
 
@@ -31,21 +30,9 @@ class Subscriber:
             qos_provider.topic_qos,
         )
 
-        # Extract key fields and create the ContentFilteredTopic
-        expressions = []
-        for key in filter_keys:
-            expr = f"(A_sourceID.A_platformId = {key.A_platformId} AND " \
-                   f"A_sourceID.A_moduleId = {key.A_moduleId} AND " \
-                   f"A_sourceID.A_systemId = {key.A_systemId})"
-            expressions.append(expr)
-
-        expression = " OR ".join(expressions)
-        filter_test = dds.Filter(expression)
-
+        filter_test = dds.Filter(self.__get_filter_expression())
         self.filtered_topic = dds.ContentFilteredTopic(self.topic, self.topic.name + "_filtered", filter_test)
 
-        # Create a DataReader with the QoS profile "Default",
-        # which is in the QoS library "Barak_Library".
         self.reader_default = dds.DataReader(
             self.subscriber,
             self.filtered_topic,
@@ -63,28 +50,14 @@ class Subscriber:
             self.__execute_subscribe_event(data)
             logger.trace(f'{data}')
 
+    def __get_filter_expression(self) -> str:
+        return " OR ".join(map(lambda key: f"(A_sourceID.A_platformId = {key.A_platformId} "
+                                           f"AND A_sourceID.A_moduleId = {key.A_moduleId} "
+                                           f"AND A_sourceID.A_systemId = {key.A_systemId})",
+                               self.filter_keys))
+
     def subscribe(self):
         try:
             rti.asyncio.run(self.__run())
         except KeyboardInterrupt:
             pass
-
-
-if __name__ == "__main__":
-    def test_event(data):
-        print(f"check the data: {str(data)}")
-
-
-    try:
-        # Create a list of P_LDM_Common_T_Identifier objects
-        filter_keys = [
-            P_LDM_Common_T_Identifier(A_platformId=1, A_systemId=14, A_moduleId=1),
-            P_LDM_Common_T_Identifier(A_platformId=1, A_systemId=21, A_moduleId=1),
-        ]
-
-        # Pass the filter keys to the ProfilesExampleSubscriber constructor
-        subscriber = Subscriber(StructEnum.DETECTION_OPTRONICS, test_event, filter_keys)
-        subscriber.subscribe()
-
-    except KeyboardInterrupt:
-        pass
