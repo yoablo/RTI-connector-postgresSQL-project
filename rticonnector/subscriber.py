7fd3c7b3f17cd96045897b60fa46cf4ce1bae1d6
Ofek Livne <ofek.livne@gmail.com>
2024-08-21 16:35:51 +0300

    fix(subscriber.py): made `__get_filter_expression(filter_keys)` static

--- rticonnector/subscriber.py
+++ rticonnector/subscriber.py
@@ -14,8 +14,6 @@ class Subscriber:
     def __init__(self, struct_enum: StructEnum, subscribe_event: Callable,
                  filter_keys: Union[list[P_LDM_Common_T_Identifier], str, None], domain_id=DEFAULT_DOMAIN_ID):
         self.struct_enum = struct_enum
-        self.filter_keys = filter_keys
-
         qos_provider = dds.QosProvider(QOS_FILE_PATH)
 
         self.participant = dds.DomainParticipant(
@@ -34,9 +32,9 @@ class Subscriber:
             if isinstance(filter_keys, str):
                 filter_expression = dds.Filter(filter_keys)
             elif isinstance(filter_keys, list):
-                filter_expression = dds.Filter(self.__get_filter_expression())
+                filter_expression = dds.Filter(self.__get_filter_expression(filter_keys))
             else:
-                raise ValueError("Invalid type for filter_keys. Must be str or list.")
+                raise ValueError("Invalid type for filter_keys. Must be str or list of P_LDM_Common_T_Identifier type")
             self.filtered_topic = dds.ContentFilteredTopic(self.topic, self.topic.name + "_filtered", filter_expression)
         else:
             self.filtered_topic = self.topic
@@ -50,14 +48,15 @@ class Subscriber:
         )
         self.subscribe_event = subscribe_event
 
-    def __execute_subscribe_event(self, *args, **kwargs):
-        return self.subscribe_event(*args, **kwargs)
-
-    def __get_filter_expression(self) -> str:
+    @staticmethod
+    def __get_filter_expression(filter_keys) -> str:
         return " OR ".join(map(lambda key: f"(A_sourceID.A_platformId = {key.A_platformId} "
                                            f"AND A_sourceID.A_moduleId = {key.A_moduleId} "
                                            f"AND A_sourceID.A_systemId = {key.A_systemId})",
-                               self.filter_keys))
+                               filter_keys))
+
+    def __execute_subscribe_event(self, *args, **kwargs):
+        return self.subscribe_event(*args, **kwargs)
 
     async def __run(self):
         async for data in self.reader_default.take_data_async():
