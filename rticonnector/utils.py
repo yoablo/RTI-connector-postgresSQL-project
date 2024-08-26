598e0fe3e799b5987535f43a6f9c63e09f3e6817
ofek.livne <ofek.livne@gmail.com>
2024-08-26 14:57:55 +0300

    feat(rticonnector\utils.py) added a few more random functions

--- rticonnector/utils.py
+++ rticonnector/utils.py
@@ -1,5 +1,19 @@
-from rti import idl
+from time import time
+from rti.idl import char
+
+from rticonnector.idl_types.TacticalSensorSpecification import P_LDM_Common_T_ShortString
 
 
-def str_to_char_sequence(str_input):
-    return [idl.char(ord(ch)) for ch in str_input]
+def str_to_char_sequence(str_input: str) -> list[char]:
+    return [char(ord(ch)) for ch in str_input]
+
+
+def str_list_to_short_string_sequence(str_list: list[str]) -> list[P_LDM_Common_T_ShortString]:
+    return [P_LDM_Common_T_ShortString(str_to_char_sequence(string)) for string in str_list]
+
+
+def get_seconds_and_nanoseconds() -> (int, int):
+    time_now = time()
+    seconds = int(time_now)
+    nanoseconds = int((time_now - seconds) * 1_000_000_000)
+    return seconds, nanoseconds
