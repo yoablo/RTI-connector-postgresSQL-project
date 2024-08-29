8fad09e8af7b23630cacdb90cf7f342af806cefa
ofek.livne <ofek.livne@gmail.com>
2024-08-29 14:46:39 +0300

    fix(rticonnector\utils.py): moved 'get_seconds_and_nanoseconds' that was used in only one file

--- rticonnector/utils.py
+++ rticonnector/utils.py
@@ -1,4 +1,3 @@
-from time import time
 from rti.idl import char
 
 from rticonnector.idl_types.TacticalSensorSpecification import P_LDM_Common_T_ShortString
@@ -10,10 +9,3 @@ def str_to_char_sequence(str_input: str) -> list[char]:
 
 def str_list_to_short_string_sequence(str_list: list[str]) -> list[P_LDM_Common_T_ShortString]:
     return [P_LDM_Common_T_ShortString(str_to_char_sequence(string)) for string in str_list]
-
-
-def get_seconds_and_nanoseconds() -> (int, int):
-    time_now = time()
-    seconds = int(time_now)
-    nanoseconds = int((time_now - seconds) * 1_000_000_000)
-    return seconds, nanoseconds
