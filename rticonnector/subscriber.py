7a784c4b5d048f931d00e9b9abd70866b985b89f
Eitan Horn <BarakDeployment@gmail.com>
2024-09-03 14:21:44 +0300

    Changed subscriber to a thread class
    Changelog: Changed

--- rticonnector/subscriber.py
+++ rticonnector/subscriber.py
@@ -1,18 +1,18 @@
-import rti.asyncio  # we are not using it directly but removing it will get en error
-from asyncio import run
-from loguru import logger
-from threading import Thread
+import threading
+from typing import Callable
+
 import rti.connextdds as dds
-from typing import Callable, Union
+from loguru import logger
 
-from rticonnector.utils import get_qos_file
-from rticonnector.topic_data import TopicEnum, topic_data_dict
 from rticonnector.constants import DEFAULT_DOMAIN_ID, PROFILE_NAME
+from rticonnector.topic_data import TopicEnum, topic_data_dict
+from rticonnector.utils import get_qos_file
 
 
-class Subscriber:
+class Subscriber(threading.Thread):
     def __init__(self, topic_enum: TopicEnum, subscribe_event: Callable, filter_str: str, qos_file_path: str = None,
                  domain_id=DEFAULT_DOMAIN_ID):
+        super().__init__(daemon=True)
         self.topic_enum = topic_enum
         qos_provider = dds.QosProvider(get_qos_file(qos_file_path))
 
@@ -29,9 +29,8 @@ class Subscriber:
             qos_provider.topic_qos,
         )
 
-        self.filtered_topic = dds.ContentFilteredTopic(self.topic, self.topic.name, dds.Filter(filter_str))\
+        self.filtered_topic = dds.ContentFilteredTopic(self.topic, self.topic.name, dds.Filter(filter_str)) \
             if filter_str != "" else self.topic
-
 
         self.reader_default = dds.DataReader(
             self.subscriber,
@@ -45,14 +44,8 @@ class Subscriber:
     def __execute_subscribe_event(self, *args, **kwargs):
         return self.subscribe_event(*args, **kwargs)
 
-    async def __run(self):
-        async for data in self.reader_default.take_data_async():
-            self.__execute_subscribe_event(self.topic_enum, data)
-            logger.trace(f'called the subscribe event with {self.topic_enum}, where {data = }')
-
-    def __subscribe_thread(self):
-        run(self.__run())
-
-    def subscribe(self):
-        subscription_thread = Thread(target=self.__subscribe_thread, daemon=True)
-        subscription_thread.start()
+    def run(self):
+        while True:
+            for data in self.reader_default.take_data():
+                self.__execute_subscribe_event(self.topic_enum, data)
+                logger.trace(f'called the subscribe event with {self.topic_enum}, where {data = }')
